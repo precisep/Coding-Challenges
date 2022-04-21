04cd2aac6c33e87856ba6b8ac7d0720a9d61876e
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-04-21 16:55:53 +0200

    Update Task_0.8.py
    Modified function

--- level_0_challenges/Task_0.8.py
+++ level_0_challenges/Task_0.8.py
@@ -2,6 +2,6 @@ def time_conversion(hours):
 	''' Function converts hours to hours and minutes '''
 
 	if hours >= 60:
-		return str(round(hours / 60)),"hours",str(hours % 60),"minutes"
+		return print(str(round(hours / 60)),"hours",str(hours % 60),"minutes")
 	else: 
-		return str(0),"hours",str(hours % 60),"minutes"
+		return print(str(0),"hours",str(hours % 60),"minutes")
