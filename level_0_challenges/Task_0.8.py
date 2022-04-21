26e023f6f66e1445839d849e107f470daacbe7f8
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-04-21 13:18:40 +0200

    Update and rename Task-0.8.py to Task_0.8.py
    Removed print statement

--- level_0_challenges/Task_0.8.py
+++ level_0_challenges/Task_0.8.py
@@ -4,6 +4,4 @@ def time_conversion(hours):
 	if hours >= 60:
 		return str(round(hours / 60)),"hours",str(hours % 60),"minutes"
 	else: 
-		return print(str(0),"hours",str(hours % 60),"minutes")
-	
-
+		return str(0),"hours",str(hours % 60),"minutes"
