36d4a03048f605d410fe09658f675afb0b39d322
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-04-21 13:14:36 +0200

    Update and rename Task-0.5.py to Task_0.5.py
    Removed built-in function, list and modified the semi-perimeter equation.

--- level_0_challenges/Task_0.5.py
+++ level_0_challenges/Task_0.5.py
@@ -1,10 +1,8 @@
 def triangle_area(first_side, second_side, third_side):
-	  
-	sides = [first_side, second_side, third_side]
 
-	semiperimeter = (1/2)*(sum(sides))
+	semiperimeter = (1/2)*(first_side + second_side + third_side)
 
 	area = (semiperimeter*((semiperimeter-first_side)*(semiperimeter-second_side)*(semiperimeter-third_side)))**(1/2)  # Heron's formula
 	
 		
-	return print(round(area,2))
+	return round(area,2)
