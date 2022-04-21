ce2bb6987b25374b59fef4dc120fb86a67bf23f1
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-04-21 13:04:04 +0200

    Delete level_1_challenges directory

--- level_1_challenges/Task-1.2.py
+++
@@ -1,7 +0,0 @@
-def check_for_3_or_multiples(first_number,second_number):
-
-    if (first_number == 3 or second_number == 3) and ((sum([first_number, second_number])) % 3 == 0):
-        return print(True)
-    else:
-         return print(False)
-
--- level_1_challenges/Task-1.3.py
+++
@@ -1,6 +0,0 @@
-def check_for_65_or_equivalent_sum(first_number, second_number):
-
-    if first_number == 65 or second_number == 65 or sum([first_number, second_number]) == 65:
-        return print(True)
-    else:
-        return print(False)
