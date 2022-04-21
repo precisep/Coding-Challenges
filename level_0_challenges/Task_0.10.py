cd4275b63d54a95542d8b3b6e68561c5fc73179f
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-04-21 13:21:25 +0200

    Update and rename Task-0.10.py to Task_0.10.py
    Removed print statement.

--- level_0_challenges/Task_0.10.py
+++ level_0_challenges/Task_0.10.py
@@ -2,5 +2,5 @@ def common_characters(string_1, string_2):
 
 	common_letters = [x for x in string_1 for y in string_2 if x == y ]
 
-	return  print("Common letters:",', '.join(set(common_letters)))
+	return  "Common letters:",', '.join(set(common_letters))
 
