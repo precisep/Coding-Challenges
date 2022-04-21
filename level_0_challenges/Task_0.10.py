cdba7b6dbb3153903c79f1536277441583bb7bec
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-04-21 16:51:46 +0200

    Update Task_0.10.py
    Modified function.

--- level_0_challenges/Task_0.10.py
+++ level_0_challenges/Task_0.10.py
@@ -1,6 +1,5 @@
 def common_characters(string_1, string_2):
 
-	common_letters = [x for x in string_1 for y in string_2 if x == y ]
+	common_letters = [x for x in string_1.lower() for y in string_2.lower() if x == y ]
 
-	return  "Common letters:",', '.join(set(common_letters))
-
+	return  print("Common letters:",', '.join(set(common_letters)))
