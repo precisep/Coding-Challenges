e02cf9dc88d71a90cd9cc0f1fe3d9718a701dd52
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2022-05-06 13:25:36 +0200

    Update and rename Task_0.5.py to Task_0_5.py
    Removed comments

--- level_0_challenges/Task_0_5.py
+++ level_0_challenges/Task_0_5.py
@@ -2,7 +2,7 @@ def triangle_area(first_side, second_side, third_side):
 
 	semiperimeter = (1/2)*(first_side + second_side + third_side)
 
-	area = (semiperimeter*((semiperimeter-first_side)*(semiperimeter-second_side)*(semiperimeter-third_side)))**(1/2)  # Heron's formula
+	area = (semiperimeter*((semiperimeter-first_side)*(semiperimeter-second_side)*(semiperimeter-third_side)))**(1/2)  
 	
 		
 	return round(area,2)
