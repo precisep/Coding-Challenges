2b8631a473032e8424868d4a8a86286f59c0dede
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2023-04-24 12:49:26 +0200

    Update Task_0_8.py
    handling pluralization

--- level_0_challenges/Task_0_8.py
+++ level_0_challenges/Task_0_8.py
@@ -1,6 +1,13 @@
-def time_conversion(hours):
+def time_conversion(minutes):
+    hours = minutes // 60
+    minutes = minutes % 60
+    
+    
+    hour_str = "hour" if hours == 1 else "hours"
+    minute_str = "minute" if minutes == 1 else "minutes"
+    
+    if hours > 0:
+        return f"{hours} {hour_str}, {minutes} {minute_str}"
+    else:
+        return f"{minutes} {minute_str}"
 
-	if hours >= 60:
-		return print(str(round(hours / 60)),"hour",",",str(hours % 60),"minutes")
-	else: 
-		return print(str(0),"hour",",",str(hours % 60),"minutes")
