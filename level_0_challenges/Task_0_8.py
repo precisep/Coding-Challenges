8891dc001a93eb6e3d69aa00bd1e874240ef43ae
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2023-04-24 12:55:48 +0200

    Update Task_0_8.py
    modified code structure

--- level_0_challenges/Task_0_8.py
+++ level_0_challenges/Task_0_8.py
@@ -2,9 +2,16 @@ def time_conversion(minutes):
     hours = minutes // 60
     minutes = minutes % 60
     
-    
-    hour_str = "hour" if hours == 1 else "hours"
-    minute_str = "minute" if minutes == 1 else "minutes"
+    if hours == 1:
+        hour_str = "hour"  
+    else :
+        hour_str = "hours"
+
+    if minutes == 1: 
+        minute_str = "minute" 
+    else:
+        
+        minute_str = "minutes"
     
     if hours > 0:
         return f"{hours} {hour_str}, {minutes} {minute_str}"
