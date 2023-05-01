6b3e55a62500140fe83aa820a942f4b0760b7142
precisep <thabangpardonmokoena@gmail.com>
2023-05-01 18:48:06 +0200

    made changes and formated with black

--- task10.py
+++
@@ -1,4 +0,0 @@
-def common_characters(string_1, string_2):
-    common_letters = [x for x in string_1.lower() for y in string_2.lower() if x == y]
-
-    return print("Common letters:", ", ".join(set(common_letters)))
