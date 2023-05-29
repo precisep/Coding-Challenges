4540aa940e1c3d283adcd90c131a26e4684baf0d
precisep <thabangpardonmokoena@gmail.com>
2023-05-29 12:43:40 +0200

    made code more consise and improved readability

--- task_08.py
+++ task_08.py
@@ -2,21 +2,11 @@ def time_conversion(minutes):
     hours = minutes // 60
     minutes = minutes % 60
 
-    if hours == 1:
-        hour_str = "hour"
-    else:
-        hour_str = "hours"
+    hour_str = "hour" if hours == 1 else "hours"
+    minute_str = "minute" if minutes == 1 else "minutes"
 
-    if minutes == 1:
-        minute_str = "minute"
-    else:
-        minute_str = "minutes"
-
-    if hours == 0:
-        hour_str = "hour"
-    if minutes == 0:
-        minute_str = "minute"
-    
+    hour_str = "hour" if hours == 0 else hour_str
+    minute_str = "minute" if minutes == 0 else minute_str
 
     return f"{hours} {hour_str}, {minutes} {minute_str}"
    
